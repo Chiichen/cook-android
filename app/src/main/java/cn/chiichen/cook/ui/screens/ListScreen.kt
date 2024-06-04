1ba7d346ae573eb0ad2a690d2775afaf57593fa5
cc0924 <92106612+H0924cc@users.noreply.github.com>
2024-06-04 16:58:25 +0800

    feat: add preview dependencies and some template code (#4)
    * some basic changes
    
    * delete useless code
    
    * add preview dependencies and some template code

--- app/src/main/java/cn/chiichen/cook/ui/screens/ListScreen.kt
+++ app/src/main/java/cn/chiichen/cook/ui/screens/ListScreen.kt
@@ -1,13 +1,63 @@
 package cn.chiichen.cook.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ListScreen() {
-    Text(text = "List Screen")
+
+    var number : Int = 1
+
+    Column(
+        modifier = Modifier.padding(12.dp)
+    ) {
+
+        Text(text = "今天吃什么",
+            fontSize = 40.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+
+            }
+
+            Text(text = "$number",
+                fontSize = 20.sp
+            )
+
+            IconButton(onClick = { /*TODO*/ }) {
+                
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun Preview(){
+    ListScreen()
 }
